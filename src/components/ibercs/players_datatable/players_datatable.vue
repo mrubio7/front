237cbd7a433a3117be334168eedfb24b719c760d
mrubio7 <manuel.rubio.arias@gmail.com>
2024-10-19 20:28:33 +0200

    fix

--- src/components/ibercs/players_datatable/players_datatable.vue
+++ src/components/ibercs/players_datatable/players_datatable.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed, watch, onMounted } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { PropType } from 'vue'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import { Avatar, AvatarImage } from '@/components/ui/avatar'
