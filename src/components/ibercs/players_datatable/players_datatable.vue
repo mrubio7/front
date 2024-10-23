36af4a42646ccae2c7040d9f372665c2484ec1a8
mrubio7 <manuel.rubio.arias@gmail.com>
2024-10-23 12:26:53 +0200

    fix

--- src/components/ibercs/players_datatable/players_datatable.vue
+++ src/components/ibercs/players_datatable/players_datatable.vue
@@ -14,7 +14,6 @@ import { useRoute, useRouter } from 'vue-router'
 
 const route = useRoute();
 const router = useRouter();
-const q = route.query["search"];
 
 // Props
 const props = defineProps({
