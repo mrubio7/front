90f6c39ee8298ce210938fec4e5c65729322dd00
mrubio7 <manuel.rubio.arias@gmail.com>
2024-10-24 14:41:05 +0200

    sxcasdasdsadsa

--- src/api/api_localstorage.ts
+++ src/api/api_localstorage.ts
@@ -1,4 +1,15 @@
 const ApiLocalStorage = {
+    Token: {
+        Save: (value:any) => {
+            localStorage.setItem("token", JSON.stringify(value))
+        },
+        Get: () => {
+            const data = localStorage.getItem("token")
+            if (data != null)
+                return JSON.parse(data)
+            return null
+        }
+    },
     Save: (key:string, value:any) => {
         localStorage.setItem(key, JSON.stringify(value))
     },
